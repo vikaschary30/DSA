f516d7166de5b4258a1c4b5ee83fe0f88fb8a0ff
Vikas <vikaschary3001@gmail.com>
2025-08-27 22:39:26 +0530

    Count reverse pairs ( brute force )

--- Arrays/Hard/Arrays41.java
+++ Arrays/Hard/Arrays41.java
@@ -1,9 +1,23 @@
 //Count reverse pairs - 1)
 
 
-
+import java.util.*;
 class Arrays41{
     public static void main(String[] args) {
-        System.out.println("count reverse pairs");   
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            for (int i = 0; i < n; i++){
+                a[i]=sc.nextInt();
+            }
+            int c=0;
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                    if(a[i]>2*a[j]){
+                        c++;
+                    }
+                }
+            }
+            System.out.println(c);
     }
 }
