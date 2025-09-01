cfc80eac0a3920b282ab1248e4b2b0f02fd2feac
Vikas <vikaschary3001@gmail.com>
2025-09-01 11:05:09 +0530

    Implementation of lower and upper bound

---
+++ Binary Search/BS on 1D/BS3.java
@@ -0,0 +1,30 @@
+// Implement Upper Bound - iterative : TC:O(LogN)
+
+
+import java.util.*;
+class BS3{
+    public static int lowerbound(int a[], int low, int high, int x, int ans) {
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(a[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for (int i = 0; i < n; i++){
+            a[i]=sc.nextInt();
+        }
+        int x=sc.nextInt();
+        int ans=n;
+        System.out.println(lowerbound(a,0,n-1,x,ans));
+    }
+}
