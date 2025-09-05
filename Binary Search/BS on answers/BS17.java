b834210dc55dad64f5960f31dabdd0c0282538b4
Vikas <vikaschary3001@gmail.com>
2025-09-05 14:26:57 +0530

    complexity approx

--- Binary Search/BS on answers/BS17.java
+++ Binary Search/BS on answers/BS17.java
@@ -1,8 +1,8 @@
 // Minimum days to make M bouquets - Problem Statemen : You are given 'N’ roses and you are also given an array 'arr'  where 'arr[i]'  denotes that the 'ith' rose will bloom on the 'arr[i]th' day.
 //                                                      You can only pick already bloomed roses that are adjacent to make a bouquet. You are also told that you require exactly 'k' adjacent bloomed roses to make a single bouquet.
 //                                                      Find the minimum number of days required to make at least ‘m' bouquets each containing 'k' roses. Return -1 if it is not possible.
-//                                  1) brute force - TC : O((max-min+1)*n) Try every day from the earliest bloom (min) to the latest bloom (max), and for each day, count how many bouquets can be formed; the first day with at least m bouquets is the answer.
-//                                  2) optimal - TC : O(log(max-min+1)*n) Search for the smallest day between min and max such that at least m bouquets of size k can be made.
+//                                  1) brute force - TC : O((max-min)*n) Try every day from the earliest bloom (min) to the latest bloom (max), and for each day, count how many bouquets can be formed; the first day with at least m bouquets is the answer.
+//                                  2) optimal - TC : O(log(max-min)*n) Search for the smallest day between min and max such that at least m bouquets of size k can be made.
 
 import java.util.*;
 class BS17{
