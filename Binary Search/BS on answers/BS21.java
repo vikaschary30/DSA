6f66b6c8c8f4eff3a899284800134a84f0cfdd20
Vikas <vikaschary3001@gmail.com>
2025-09-07 22:04:23 +0530

    approach description change

--- Binary Search/BS on answers/BS21.java
+++ Binary Search/BS on answers/BS21.java
@@ -1,8 +1,8 @@
 // Aggressive Cows - Problem statement : You are given an array 'arr' of size 'n' which denotes the position of stalls.You are also given an integer 'k' which denotes the number of aggressive cows.
 //                                       You are given the task of assigning stalls to 'k' cows such that the minimum distance between any two of them is the maximum possible.
 //                                       Find the maximum possible minimum distance.
-//                   1) brute force - TC:nlogn+O((max-min)*n) Try every possible minimum distance from 1 to (max - min) and check for each if k cows can be placed in the stalls, keeping the last valid distance as the answer.
-//                   2) optimal - TC:nlogn+O(log(max-min)*n) Use binary search on the distance range (1 to max-min), and for each mid, check if k cows can be placed with at least that distance; maximize the valid distance.
+//                   1) brute force - TC:nlogn+O((max-min)*n) After sorting the array, Try every possible minimum distance from 1 to (max - min) and check for each if k cows can be placed in the stalls, keeping the last valid distance as the answer.
+//                   2) optimal - TC:nlogn+O(log(max-min)*n) After sorting the array, Use binary search on the distance range (1 to max-min), and for each mid, check if k cows can be placed with at least that distance; maximize the valid distance.
 
 import java.util.*;
 class BS21{
