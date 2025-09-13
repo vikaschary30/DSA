03b955ccd5156754488aca0ea0a344a81a40a1e4
Vikas <vikaschary3001@gmail.com>
2025-09-13 21:56:43 +0530

    small optimization in median of two arrays

--- Binary Search/BS on answers/BS26.java
+++ Binary Search/BS on answers/BS26.java
@@ -14,22 +14,10 @@ class BS26{
         while(low<=high){
             int mid1=(low+high)/2;
             int mid2=left-mid1;
-            int l1=Integer.MIN_VALUE;
-            int l2=Integer.MIN_VALUE;
-            int r1=Integer.MAX_VALUE;
-            int r2=Integer.MAX_VALUE;
-            if(mid1>0){
-                l1=a[mid1-1];
-            }
-            if(mid2>0){
-                l2=b[mid2-1];
-            }
-            if(mid1<n1){
-                r1=a[mid1];
-            }
-            if(mid2<n2){
-                r2=b[mid2];
-            }
+            int l1=(mid1>0) ? a[mid1-1] : Integer.MIN_VALUE;
+            int l2=(mid2>0) ? b[mid2-1] : Integer.MIN_VALUE;
+            int r1=(mid1<n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2=(mid2<n2) ? b[mid2] : Integer.MAX_VALUE;
             if(l1<=r2 && l2<=r1)
             {
                 if(n%2==0){
