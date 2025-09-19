e21ad978289c9d732ad9b1b9c6f7f3f58b629814
Vikas <vikaschary3001@gmail.com>
2025-09-19 22:44:13 +0530

    Longest Common Prefix optimal

--- Strings/Basic and Easy Problems/S4.java
+++ Strings/Basic and Easy Problems/S4.java
@@ -1,5 +1,6 @@
 // Longest Common Prefix - Problem Statement : Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
-//                         1) optimal : TC:O(N*M*logN)
+//                         1) better : TC:O(N*M*logN)
+//                         2) optimal : TC:O(N*M)
 import java.util.*;
 class S4{
     public static void main(String[] args) {
@@ -9,19 +10,26 @@ class S4{
         for(int i=0;i<n;i++){
             s[i]=sc.next();
         }
-        Arrays.sort(s);
-        String s1=s[0];
-        String s2=s[n-1];
-        int p1=0;
-        while(p1 < s1.length() && p1 < s2.length() && s1.charAt(p1) == s2.charAt(p1)){
-            p1++;
-        }
-        System.out.println(s1.substring(0,p1));
+        // Arrays.sort(s);
+        // String s1=s[0];
+        // String s2=s[n-1];
+        // int p1=0;
+        // while(p1 < s1.length() && p1 < s2.length() && s1.charAt(p1) == s2.charAt(p1)){
+        //     p1++;
+        // }
+        // System.out.println(s1.substring(0,p1));
 
         //
-        // String s1=s[0];
-        // for(int i=1;i<n;i++){
-        //     while(!)
-        // }
+        String s1=s[0];
+        for(int i=1;i<n;i++){
+            while(!s[i].startsWith(s1)){
+                s1=s1.substring(0, s1.length()-1);
+                if(s1.equals("")){
+                    System.out.println("");
+                    return;
+                }
+            }
+        }
+        System.out.println(s1);
     }
 }
