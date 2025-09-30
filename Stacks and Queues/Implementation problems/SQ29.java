c9139895a7c6a53e6387f488456521df6750767e
Vikas <vikaschary3001@gmail.com>
2025-09-30 15:56:29 +0530

    Celebrity Problem (optimal)

--- Stacks and Queues/Implementation problems/SQ29.java
+++ Stacks and Queues/Implementation problems/SQ29.java
@@ -1,6 +1,6 @@
 // Celebrity Problem - Problem Statement : A celebrity is a person who is known by everyone else at the party but does not know anyone in return. Given a square matrix M of size N x N where M[i][j] is 1 if person i knows person j, and 0 otherwise, determine if there is a celebrity at the party. Return the index of the celebrity or -1 if no such person exists.
 //                     1) brute force - TC:O(N^2) SC:O(2N) Count for each person how many know them and how many they know; a celebrity is someone known by everyone else (knowme[i] = n-1) and knows no one (iknow[i] = 0).
-//                     2) optimal - 
+//                     2) optimal - TC:O(N) SC:O(1) Use two pointers to eliminate non-celebrities; the remaining candidate is checked against all others to confirm celebrity status.
 
 
 import java.util.*;
@@ -16,22 +16,41 @@ class SQ29{
         }
 
         //brute force:
-        int[] knowme=new int[n];
-        int[] iknow=new int[n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(a[i][j]==1){
-                    knowme[j]++;
-                    iknow[i]++;
-                }
+        // int[] knowme=new int[n];
+        // int[] iknow=new int[n];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(a[i][j]==1){
+        //             knowme[j]++;
+        //             iknow[i]++;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(knowme[i]==n-1 && iknow[i]==0){
+        //         System.out.println(i);
+        //         return;
+        //     }
+        // }
+
+
+        // optimal:
+        int top=0, down=n-1;
+        while(top<down){
+            if(a[top][down]==1){
+                top++;
+            }
+            else {
+                down--;
             }
         }
         for(int i=0;i<n;i++){
-            if(knowme[i]==n-1 && iknow[i]==0){
-                System.out.println(i);
+            if(top==i) continue;
+            if(a[top][i]!=0 || a[i][top]!=1){
+                System.out.println("-1");
                 return;
             }
         }
-        System.out.println("-1");
+        System.out.println(top);
     }
 }
