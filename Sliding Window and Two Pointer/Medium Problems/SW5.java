bd4a2509028ea9f1cf0537a6b1cd78d3f50116a9
Vikas <vikaschary3001@gmail.com>
2025-10-04 14:43:53 +0530

    Binary subarray with sum ( brute force)

---
+++ Sliding Window and Two Pointer/Medium Problems/SW5.java
@@ -0,0 +1,28 @@
+// Binary subarray with sum - Problem Statement : You are given a binary array nums (containing only 0s and 1s) and an integer goal. Return the number of non-empty subarrays of nums that sum to goal. A subarray is a contiguous part of the array.
+//                            1) brute force -
+
+import java.util.*;
+class SW5{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+
+        //brute force:
+        int c=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=a[j];
+                if(sum==k){
+                    c++;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
