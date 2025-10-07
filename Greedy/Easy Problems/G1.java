e3e527c00102f0a5b6d7b50050f7c2f5af3077d4
Vikas <vikaschary3001@gmail.com>
2025-10-07 22:26:07 +0530

    Greedy Algorithms

---
+++ Greedy/Easy Problems/G1.java
@@ -0,0 +1,14 @@
+// Assign Cookies - Problem Statement : Consider a scenario where a teacher wants to distribute cookies to students, with each student receiving at most one cookie. Given two arrays, student and cookie, the ith value in the student array describes the minimum size of cookie that the ith student can be assigned. The jth value in the cookie array represents the size of the jth cookie. If cookie[j] >= student[i], the jth cookie can be assigned to the ith student. Maximize the number of students assigned with cookies and output the maximum number.
+//                  1) brute force -
+
+import java.util.*;
+
+class G1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int Student[] = new int[n];
+        int m=sc.nextInt();
+        int Cookie[] = new int[m];
+    }
+}
