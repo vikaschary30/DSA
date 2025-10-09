fb93e93dd470ea81d22f28a4f640c86e211897a0
Vikas <vikaschary3001@gmail.com>
2025-10-09 22:46:29 +0530

    space complexity

--- Greedy/Easy Problems/G3.java
+++ Greedy/Easy Problems/G3.java
@@ -1,5 +1,5 @@
 // Find minimum number of coins - Problem Statement : Given a value V, if we want to make a change for V Rs, and we have an infinite supply of each of the denominations in Indian currency, i.e., we have an infinite supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, what is the minimum number of coins and/or notes needed to make the change.
-//                                1) optimal - Greedy Approach : TC:O(worst-(v/smallest_denomination)  or  avg-(1)) always pick the largest denomination less than or equal to the remaining value until the total sum equals the target amount.
+//                                1) optimal - Greedy Approach : TC:O(worst-(v/smallest_denomination)  or  avg-(1)) SC:O(deno.length) always pick the largest denomination less than or equal to the remaining value until the total sum equals the target amount.
 
 import java.util.*;
 class G3{
