8c1a811a2c45d95506701a808a94a0f255f185a7
Vikas <vikaschary3001@gmail.com>
2025-10-10 16:06:50 +0530

    Jump Game 2(brute force)

---
+++ Greedy/Easy Problems/G8.java
@@ -0,0 +1,31 @@
+// Jump Game 2 - Problem Statement : You are given a 0-indexed array nums of length n representing your maximum jump capability from each index.
+//                                   You start at index 0. Each element nums[i] represents the maximum number of steps you can jump forward from index i.
+//                                   Your goal is to reach the last index of the array (nums[n - 1]) using the minimum number of jumps
+//                                   Return the minimum number of jumps required to reach the last index.You can assume that it is always possible to reach the last index.
+//               1) brute force - TC:O(N^N) SC:O(N) Recursively explore every possible jump from each index and return the minimum number of jumps needed to reach or surpass the last index.
+ 
+
+
+import java.util.*;
+class G8{
+    public static int noofjumps(int[] a, int ind, int n, int jumps){
+        if(ind>=n-1) return jumps;
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<=a[ind];i++){
+            min=Math.min(min,noofjumps(a, ind+i, n, jumps+1));
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i]=sc.nextInt();
+        }
+
+        //brute force:
+        int jumps=0,ind=0;
+        System.out.println(noofjumps(a,ind,n,jumps));
+    }
+}
