516fd0532d83bbb0841d9728cda725cb26c22367
Vikas <vikaschary3001@gmail.com>
2025-10-10 16:10:24 +0530

    Jump Game 2

--- Greedy/Easy Problems/G8.java
+++ Greedy/Easy Problems/G8.java
@@ -3,7 +3,7 @@
 //                                   Your goal is to reach the last index of the array (nums[n - 1]) using the minimum number of jumps
 //                                   Return the minimum number of jumps required to reach the last index.You can assume that it is always possible to reach the last index.
 //               1) brute force - TC:O(N^N) SC:O(N) Recursively explore every possible jump from each index and return the minimum number of jumps needed to reach or surpass the last index.
- 
+//               2) optimal - TC:O(N) SC:O(1) Greedily expand the current jump range to the farthest reachable index and count jumps whenever the current range ends until reaching the last index.
 
 
 import java.util.*;
@@ -25,7 +25,21 @@ class G8{
         }
 
         //brute force:
-        int jumps=0,ind=0;
-        System.out.println(noofjumps(a,ind,n,jumps));
+        // int jumps=0,ind=0;
+        // System.out.println(noofjumps(a,ind,n,jumps));
+
+        //optimal:
+        int l=0,r=0;
+        int jumps=0;
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++){
+                farthest=Math.max(farthest,i+a[i]);
+            }
+            jumps++;
+            l=r+1;
+            r=farthest;
+        }
+        System.out.println(jumps);
     }
 }
