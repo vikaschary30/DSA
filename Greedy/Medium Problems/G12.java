d7f409691c3fac89eab7f835a53b219814a17e99
Vikas <vikaschary3001@gmail.com>
2025-10-12 22:10:58 +0530

    Merge Overlapping Sub-intervals  (brute force)

---
+++ Greedy/Medium Problems/G12.java
@@ -0,0 +1,41 @@
+// Merge Overlapping Sub-intervals - Problem Statement : Given an array of intervals, merge all the overlapping intervals and return an array of non-overlapping intervals.
+//                                   1) brute force - 
+
+
+import java.util.*;
+class G12{
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length; 
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) { 
+            int start = arr[i][0];
+            int end = arr[i][1];
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                } else {
+                    break;
+                }
+            }
+            ans.add(Arrays.asList(start, end));
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        List<List<Integer>> ans = mergeOverlappingIntervals(arr);
+        System.out.print("The merged intervals are: \n");
+        for (List<Integer> it : ans) {
+            System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
+        }
+        System.out.println();
+    }
+}
