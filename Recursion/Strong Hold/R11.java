5afca6c8ad2cfccc421ff810ca8310e2ca4e1b8b
Vikas <vikaschary3001@gmail.com>
2025-10-17 15:09:43 +0530

    Count Good numbers (TC & SC)

--- Recursion/Strong Hold/R11.java
+++ Recursion/Strong Hold/R11.java
@@ -1,5 +1,5 @@
 // Count Good numbers - Problem Statement :  A digit string is considered good if the digits at even indices (0-based) are even digits (0, 2, 4, 6, 8) and the digits at odd indices are prime digits (2, 3, 5, 7).Given an integer n, return the total number of good digit strings of length n. As the result may be large, return it modulo 109 + 7.A digit string is a string consisting only of the digits '0' through '9'. It may contain leading zeros.
-//                      1) optimal - 
+//                      1) optimal - TC:O(LogN) SC:O(LogN) counts all “good” numbers by computing the total combinations of even and prime digits at respective positions using fast modular exponentiation.
 
 import java.util.*;
 class R11{
