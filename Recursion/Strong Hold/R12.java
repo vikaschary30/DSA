b0924f8564183d06b5274cc28c4bce56e1362fb4
Vikas <vikaschary3001@gmail.com>
2025-10-18 22:09:33 +0530

    Recursion rev

---
+++ Recursion/Strong Hold/R12.java
@@ -0,0 +1,7 @@
+// Sort a Stack - Problem Statement : You are given a stack of integers. Your task is to sort the stack in descending order using recursion, such that the top of the stack contains the greatest element. You are not allowed to use any loop-based sorting methods (e.g., quicksort, mergesort). You may only use recursive operations and the standard stack operations (push, pop, peek/top, and isEmpty).
+//                1) optimal -
+class R12{
+    public static void main(String[] args) {
+        
+    }
+}
