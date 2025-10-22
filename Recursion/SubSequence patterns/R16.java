60419e138d59ff35183d91f9298315f20c228548
Vikas <vikaschary3001@gmail.com>
2025-10-22 16:12:22 +0530

    Print all the possible subsequences of the String

---
+++ Recursion/SubSequence patterns/R16.java
@@ -0,0 +1,43 @@
+// Print all the possible subsequences of the String - Problem Statement : Given a string, find all the possible subsequences of the string.
+//                                                     1) brute force - TC:O(2^N*N) SC:O(2^N*N) Generate all 2^n binary masks and form subsequences based on bits.
+//                                                     2) optimal - TC:O(2^N*N) SC:O(2^N*N) Recursively include or exclude each character to build subsequences
+
+
+import java.util.*;
+class R16{
+    public static void generateSubsequences(String s, int n, int ind, StringBuilder curr, List<String> res){
+        if(ind==n){
+            res.add(curr.toString());
+            return;
+        }
+        curr.append(s.charAt(ind));
+        generateSubsequences(s, n, ind+1, curr, res);
+        curr.deleteCharAt(curr.length()-1);
+        generateSubsequences(s, n, ind+1, curr, res);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int n=s.length();
+
+        //brute force:
+        // List<String> res=new ArrayList<>();
+        // for(int i=0;i<Math.pow(2,n)-1;i++){
+        //     StringBuilder temp=new StringBuilder();
+        //     for(int j=0;j<n;j++){
+        //         if(((1<<j)&i)!=0){
+        //             temp.append(s.charAt(j));
+        //         }
+        //     }
+        //     res.add(temp.toString());
+        // }
+        // System.out.println(res);
+
+
+        //optimal:
+        List<String> res=new ArrayList<>();
+        StringBuilder curr=new StringBuilder();
+        generateSubsequences(s,n,0,curr,res);
+        System.out.println(res);
+    }
+}
